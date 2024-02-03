bdb6d89f6b58c11cfb3ab876a06c5a569a219ec0
Fillpz <filipepinho2010@hotmail.com>
2024-02-02 21:44:35 -0300

    Finalizando logica de vencedor ou velha

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let tabuleiro = ['','','','','','','','',''];
 let turno = 0;
-const simbolo = ['\u2694\uFE0F','\u2B55']; //Ajustar 
+const jogador = ['\u2694\uFE0F','\u2B55'];
  
 /* Após carregar toda a DOM e espera um click em uma 
     das casas para executar clickNoQuadrado */
@@ -16,11 +16,58 @@ function clickNoQuadrado(evento) {
     let casa = evento.target;
     let idDaCasaString = evento.target.id;
     let idDaCasaInt = converterIdParaInteiro(idDaCasaString);
+    
     if(tabuleiro[idDaCasaInt] == '') {
-        preencherTabuleiro(idDaCasaInt, simbolo[turno]);
-        casa.innerText = simbolo[turno];
+        preencherTabuleiro(idDaCasaInt, jogador[turno]);
+        casa.innerText = jogador[turno];
         trocarJogador();
+        verificarVencedor();
     }
+}
+
+function verificarVencedor() {
+    let xGanhou = (
+        // Verificando vertical
+          (tabuleiro[0] == jogador[0] && tabuleiro[3] == jogador[0] && tabuleiro[6] == jogador[0])
+        ||(tabuleiro[1] == jogador[0] && tabuleiro[4] == jogador[0] && tabuleiro[7] == jogador[0])
+        ||(tabuleiro[2] == jogador[0] && tabuleiro[5] == jogador[0] && tabuleiro[8] == jogador[0])
+    
+        // Verificando horizontal
+        ||(tabuleiro[0] == jogador[0] && tabuleiro[1] == jogador[0] && tabuleiro[2] == jogador[0])
+        ||(tabuleiro[3] == jogador[0] && tabuleiro[4] == jogador[0] && tabuleiro[5] == jogador[0])
+        ||(tabuleiro[6] == jogador[0] && tabuleiro[7] == jogador[0] && tabuleiro[8] == jogador[0])
+    
+        // Verificando diagonal
+        ||(tabuleiro[0] == jogador[0] && tabuleiro[4] == jogador[0] && tabuleiro[8] == jogador[0])
+        ||(tabuleiro[2] == jogador[0] && tabuleiro[4] == jogador[0] && tabuleiro[6] == jogador[0])
+    );
+    let oGanhou = (
+        // Verificando vertical
+        (tabuleiro[0] == jogador[1] && tabuleiro[3] == jogador[1] && tabuleiro[6] == jogador[1])
+        ||(tabuleiro[1] == jogador[1] && tabuleiro[4] == jogador[1] && tabuleiro[7] == jogador[1])
+        ||(tabuleiro[2] == jogador[1] && tabuleiro[5] == jogador[1] && tabuleiro[8] == jogador[1])
+    
+        // Verificando horizontal
+        ||(tabuleiro[0] == jogador[1] && tabuleiro[1] == jogador[1] && tabuleiro[2] == jogador[1])
+        ||(tabuleiro[3] == jogador[1] && tabuleiro[4] == jogador[1] && tabuleiro[5] == jogador[1])
+        ||(tabuleiro[6] == jogador[1] && tabuleiro[7] == jogador[1] && tabuleiro[8] == jogador[1])
+    
+        // Verificando diagonal
+        ||(tabuleiro[0] == jogador[1] && tabuleiro[4] == jogador[1] && tabuleiro[8] == jogador[1])
+        ||(tabuleiro[2] == jogador[1] && tabuleiro[4] == jogador[1] && tabuleiro[6] == jogador[1])
+    );
+
+    if(xGanhou){
+        console.log(jogador[0] + 'ganhou');
+    } else if(oGanhou){
+        console.log(jogador[1] + 'ganhou');
+    } else if(tabuleiroCompleto()){
+        console.log('Deu velha');
+    }
+}
+
+function tabuleiroCompleto() {
+    return tabuleiro.every(casa => casa !== '');
 }
 
 // Inverte o valor do id da div de string para int
@@ -34,7 +81,6 @@ function preencherTabuleiro(casaClickada, jogador){
     if(tabuleiro[casaClickada] == ''){
         tabuleiro[casaClickada] = jogador;
     }
-    console.log(tabuleiro);
 }
 
 // Inverte o turno de jogador
